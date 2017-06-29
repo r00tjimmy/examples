8155556689acf24df9033deb2a185102b0f90873
r00txx <82049406@qq.com>
2017-06-29 15:39:05 +0800

    add ffive api

--- puffin/cicd/run.sh
+++ puffin/cicd/run.sh
@@ -39,6 +39,17 @@ curl -H 'Content-Type: application/json' \
 
 
 
+用这个东西来写API的流程:
+
+1.  定义好接口服务，也就是 srv/proto 下面的proto 文件，生成接口服务的go代码文件
+2.  编写srv下面的main.go， 为接口定义具体的逻辑
+3.  跟srv同一级目录建立api目录，其中api目录可以分别建立rest和rpc两个目录， rest是指提供支持HTTP和通讯型RPC调用的功能，  rpc目录是提供源码级（例如python 客户端代码）的服务。 该目录的api.go 是声明该api的路由的，该路由应用于rest和rpc两个文件夹都生效。
+
+
+
+
+
+
 
 
 
